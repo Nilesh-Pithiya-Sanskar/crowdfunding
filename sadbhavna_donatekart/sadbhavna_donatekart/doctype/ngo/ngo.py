4016075a652caf9dbc62f442c8fb4c14e62cd2cd
Pithiya-Nilesh <112484355+Pithiya-Nilesh@users.noreply.github.com>
2023-04-03 17:20:22 +0530

    Revert "added donation campaign qty"

--- sadbhavna_donatekart/sadbhavna_donatekart/doctype/ngo/ngo.py
+++ sadbhavna_donatekart/sadbhavna_donatekart/doctype/ngo/ngo.py
@@ -8,9 +8,9 @@ from frappe.utils import get_datetime_str, formatdate
 
 
 class NGO(Document):
-	# def autoname(self):
-	# 	_date = formatdate(get_datetime_str(self.creation), "dd-mm-yyyy")
-	# 	org_name = self.organisation_name.strip()[0:5].replace(" ","-") if self.requester_type == 'NGO' else self.full_name.strip()[0:5]
-	# 	autoname = "{0}-{1}-{2}-".format(self.requester_type.strip()[0:3],org_name,_date)
-	# 	self.name = autoname.upper()+getseries(org_name, 2)
+	def autoname(self):
+		_date = formatdate(get_datetime_str(self.creation), "dd-mm-yyyy")
+		org_name = self.organisation_name.strip()[0:5].replace(" ","-") if self.requester_type == 'NGO' else self.full_name.strip()[0:5]
+		autoname = "{0}-{1}-{2}-".format(self.requester_type.strip()[0:3],org_name,_date)
+		self.name = autoname.upper()+getseries(org_name, 2)
 	pass
