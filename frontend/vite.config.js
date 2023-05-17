7347542dd6a6fc5398007c20aa5c8d252889aa79
Pithiya-Nilesh <pithiyanilesh87@gmail.com>
2023-05-17 20:19:53 +0530

    asdf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -20,7 +20,7 @@ import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Sitemap({ exclude: [] }), Pages({
+  plugins: [vue(), Sitemap({ exclude: ['/'] }), Pages({
     onRoutesGenerated: routes => (generateSitemap({ routes})),
   }),],
   server: {
