514d506b4b22faef42ca37c79887392e14b2a6b9
Pithiya-Nilesh <pithiyanilesh87@gmail.com>
2023-05-17 20:22:31 +0530

    asdf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -20,9 +20,7 @@ import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Sitemap({ exclude: ['/'] }), Pages({
-    onRoutesGenerated: routes => (generateSitemap({ routes})),
-  }),],
+  plugins: [vue(), ],
   server: {
     port: 8080,
     proxy: getProxyOptions({
